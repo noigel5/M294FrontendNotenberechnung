ed03f1f6e497b4655a9cba7ca860f7a06822ecf9
WS111 <flavio.fiechter@bbzbl-it.ch>
2023-05-25 15:56:01 +0200

    angular material first design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SemesterComponent } from './semester/semester.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { SemesterComponent } from './semester/semester.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule ,
+    MatToolbarModule,
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/semester/semester.component.ts
+++ src/app/semester/semester.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './semester.component.html',
   styleUrls: ['./semester.component.scss']
 })
-export class SemesterComponent {
 
+export class SemesterComponent {
+  displayedColumns: string[] = ['id', 'name', 'actions'];
+  dataSource = ELEMENT_DATA;
 }
+
+export interface PeriodicElement {
+  id: number;
+  name: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {id: 1, name: 'Semester 1'}
+];
