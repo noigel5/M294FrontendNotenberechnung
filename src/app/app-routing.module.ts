9ed1a34722a6fe28423395115a4b664c3e95faa3
WS111 <flavio.fiechter@bbzbl-it.ch>
2023-05-26 10:16:21 +0200

    routerOutlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SemesterComponent } from './pages/semester/semester.component';
+import { CalculatorComponent } from './pages/calculator/calculator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SemesterComponent},
+  {path: 'semester', component: SemesterComponent},
+  {path: 'calculator', component: CalculatorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {}
+export class AppComponent {
+  constructor(public oauthService: OAuthService) {
+
+  }
+}
